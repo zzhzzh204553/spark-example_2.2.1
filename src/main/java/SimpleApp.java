9c6f65a2b81f6b2e0525bf6ac6f261ec8a32c4bc
zzhzzh204553 <zhouzhenhua1984@163.com>
2018-02-05 16:21:41 +0800

    init

---
+++ src/main/java/SimpleApp.java
@@ -0,0 +1,21 @@
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+
+public class SimpleApp {
+	public static void main(String[] args) {
+		String logFile = "examples/README.md"; // Should be some file on your system
+
+		System.out.println(logFile);
+
+		SparkSession spark = SparkSession.builder().appName("Simple Application").config("spark.master", "local")
+				.getOrCreate();
+		Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+		long numAs = logData.filter(s -> s.contains("a")).count();
+		long numBs = logData.filter(s -> s.contains("b")).count();
+
+		System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+		spark.stop();
+	}
+}
---
+++ src/main/java/org/apache/spark/examples/streaming/StreamingExamples.java
@@ -0,0 +1,10 @@
+package org.apache.spark.examples.streaming;
+
+public class StreamingExamples {
+
+	public static void setStreamingLogLevels() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
